4d89a3fa007309a28db21074f1329b7ad9c70d43
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-05 08:50:33 +0000

    version 0.2.7 from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/taxajax/trunk@26187 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "taxajax"
+#
+# Auto generated 08-10-2009 14:36
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TYPO3 adapted xaJax',
+	'description' => 'Enhancement to the xajax extension with TYPO3 specific code. See ECT list.',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '0.2.7',
+	'dependencies' => '',
+	'conflicts' => 'xajax',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Jared White, J. Max Wilson, Franz Holzinger',
+	'author_email' => 'franz@ttproducts.de',
+	'author_company' => 'ECT (Extension Coordination Team)',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+			'xajax' => '',
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"11c6";s:11:"LICENSE.txt";s:4:"0cce";s:10:"README.txt";s:4:"c43a";s:20:"class.tx_taxajax.php";s:4:"1796";s:29:"class.tx_taxajax_response.php";s:4:"77d1";s:12:"ext_icon.gif";s:4:"3ec5";s:8:"icon.gif";s:4:"4aa7";s:17:"xajaxCompress.php";s:4:"9399";s:14:"doc/manual.sxw";s:4:"c5fb";s:17:"xajax_js/xajax.js";s:4:"ac78";s:30:"xajax_js/xajax_uncompressed.js";s:4:"6f80";}',
+	'suggests' => array(
+	),
+);
+
+?>
