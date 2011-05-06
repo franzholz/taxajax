bdf69efd69b88e1c02210ce9375c718bb241429a
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-06 11:28:42 +0000

    version 0.2.8 from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/taxajax/trunk@47367 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "taxajax"
+# Extension Manager/Repository config file for ext "taxajax".
 #
-# Auto generated 08-10-2009 14:36
+# Auto generated 06-05-2011 13:27
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Enhancement to the xajax extension with TYPO3 specific code. See ECT list.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.2.7',
+	'version' => '0.2.8',
 	'dependencies' => '',
 	'conflicts' => 'xajax',
 	'priority' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"11c6";s:11:"LICENSE.txt";s:4:"0cce";s:10:"README.txt";s:4:"c43a";s:20:"class.tx_taxajax.php";s:4:"1796";s:29:"class.tx_taxajax_response.php";s:4:"77d1";s:12:"ext_icon.gif";s:4:"3ec5";s:8:"icon.gif";s:4:"4aa7";s:17:"xajaxCompress.php";s:4:"9399";s:14:"doc/manual.sxw";s:4:"c5fb";s:17:"xajax_js/xajax.js";s:4:"ac78";s:30:"xajax_js/xajax_uncompressed.js";s:4:"6f80";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"ec27";s:11:"LICENSE.txt";s:4:"eb72";s:10:"README.txt";s:4:"4d43";s:20:"class.tx_taxajax.php";s:4:"cf8d";s:29:"class.tx_taxajax_response.php";s:4:"1866";s:12:"ext_icon.gif";s:4:"3ec5";s:8:"icon.gif";s:4:"4aa7";s:17:"xajaxCompress.php";s:4:"2d3e";s:14:"doc/manual.sxw";s:4:"f87f";s:17:"xajax_js/xajax.js";s:4:"ac78";s:30:"xajax_js/xajax_uncompressed.js";s:4:"c471";}',
 	'suggests' => array(
 	),
 );
