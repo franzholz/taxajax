9104597e94bdc338ec9dbaa3ac5c945d075fd3b4
Franz Holzinger <holzinger@ttproducts.de>
2017-04-28 20:06:44 +0200

    * use PHP5 constructor __construct * remove XCLASS for TYPO3 4.x

--- class.tx_taxajax.php
+++ class.tx_taxajax.php
@@ -155,7 +155,7 @@ class tx_taxajax
 	 * @param string  defaults to XAJAX_DEFAULT_CHAR_ENCODING defined above
 	 * @param boolean defaults to false
 	 */
-	public function tx_taxajax ($sRequestURI = '', $sWrapperPrefix = 'taxajax_', $sEncoding = XAJAX_DEFAULT_CHAR_ENCODING, $bDebug = false) {
+	public function __construct ($sRequestURI = '', $sWrapperPrefix = 'taxajax_', $sEncoding = XAJAX_DEFAULT_CHAR_ENCODING, $bDebug = false) {
 		$this->aFunctions = array();
 		$this->aObjects = array();
 		$this->aFunctionIncludeFiles = array();
@@ -1167,7 +1167,4 @@ function xajaxErrorHandler ($errno, $errstr, $errfile, $errline) {
 	$GLOBALS['xajaxErrorHandlerText'] .= '\n----\n[' . $errTypeStr . '] ' . $errstr . '\nerror in line ' . $errline . 'of file ' . $errfile;
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/taxajax/class.tx_taxajax.php']) {
-  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/taxajax/class.tx_taxajax.php']);
-}
 
