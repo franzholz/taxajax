8a8afd797de9e37c1a66f06629fa2102fc69d47d
Franz Holzinger <holzinger@ttproducts.de>
2020-11-12 18:55:08 +0100

    -

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,6 @@
 <?php
-defined('TYPO3_MODE') or die('Access denied.');
+defined('TYPO3_MODE') || die('Access denied.');
 
 if (!defined ('TAXAJAX_EXT')) {
     define('TAXAJAX_EXT', 'taxajax');
 }
-
-
