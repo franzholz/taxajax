4a71eeb3c9b44230477b1243ac8061befbca01f1
Franz Holzinger <franz.holzinger@festwein.de>
2023-12-28 17:21:44 +0100

    next version 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'TYPO3 adapted xaJax',
     'description' => 'Enhancement to the xajax extension with TYPO3 specific code.',
     'category' => 'misc',
-    'version' => '0.8.3',
+    'version' => '1.0.0',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
     'author' => 'Jared White, J. Max Wilson, Franz Holzinger',
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'php' => '7.4.0-8.4.99',
             'typo3' => '10.4.0-12.4.99',
-			'div2007' => '1.15.0-1.18.99',
+			'div2007' => '1.18.0-2.99.99',
         ],
         'suggests' => [
         ],
