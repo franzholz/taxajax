d9bc221998fa8ccf3be008c9929a5f1c294eb0a3
Franz Holzinger <franz.holzinger@festwein.de>
2025-02-14 17:03:50 +0100

    Replace _detectURI by TYPO3 getRequestUri

--- class.tx_taxajax.php
+++ class.tx_taxajax.php
@@ -146,7 +146,8 @@ class tx_taxajax
         $this->aFunctionIncludeFiles = [];
         $this->sRequestURI = $sRequestURI;
         if ($this->sRequestURI == '') {
-            $this->sRequestURI = $this->_detectURI();
+            $this->sRequestURI =
+                $GLOBALS['TYPO3_REQUEST']->getAttribute('normalizedParams')->getRequestUri();
         }
         $this->sWrapperPrefix = $sWrapperPrefix;
         $this->bDebug = $bDebug;
@@ -864,90 +865,6 @@ class tx_taxajax
                 fclose($fH);
             }
         }
-    }
-
-    /**
-     * Returns the current URL based upon the SERVER vars.
-     *
-     * @access private
-     * @return string
-     */
-    public function _detectURI()
-    {
-        $aURL = [];
-
-        // Try to get the request URL
-        if (!empty($_SERVER['REQUEST_URI'])) {
-            $aURL = parse_url($_SERVER['REQUEST_URI']);
-        }
-
-        // Fill in the empty values
-        if (empty($aURL['scheme'])) {
-            if (!empty($_SERVER['HTTP_SCHEME'])) {
-                $aURL['scheme'] = $_SERVER['HTTP_SCHEME'];
-            } else {
-                $aURL['scheme'] = (!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) != 'off') ? 'https' : 'http';
-            }
-        }
-
-        if (empty($aURL['host'])) {
-            if (!empty($_SERVER['HTTP_HOST'])) {
-                if (strpos($_SERVER['HTTP_HOST'], ':') > 0) {
-                    [$aURL['host'], $aURL['port']] = explode(':', $_SERVER['HTTP_HOST']);
-                } else {
-                    $aURL['host'] = $_SERVER['HTTP_HOST'];
-                }
-            } elseif (!empty($_SERVER['SERVER_NAME'])) {
-                $aURL['host'] = $_SERVER['SERVER_NAME'];
-            } else {
-                print 'xajax Error: xajax failed to automatically identify your Request URI.';
-                print 'Please set the Request URI explicitly when you instantiate the xajax object.';
-                exit();
-            }
-        }
-
-        if (empty($aURL['port']) && !empty($_SERVER['SERVER_PORT'])) {
-            $aURL['port'] = $_SERVER['SERVER_PORT'];
-        }
-
-        if (empty($aURL['path'])) {
-            if (!empty($_SERVER['PATH_INFO'])) {
-                $sPath = parse_url($_SERVER['PATH_INFO']);
-            } else {
-                $sPath = parse_url($_SERVER['PHP_SELF']);
-            }
-            $aURL['path'] = $sPath['path'];
-            unset($sPath);
-        }
-
-        if (!empty($aURL['query'])) {
-            $aURL['query'] = '?' . $aURL['query'];
-        }
-
-        // Build the URL: Start with scheme, user and pass
-        $sURL = $aURL['scheme'].'://';
-        if (!empty($aURL['user'])) {
-            $sURL .= $aURL['user'];
-            if (!empty($aURL['pass'])) {
-                $sURL .= ':' . $aURL['pass'];
-            }
-            $sURL .= '@';
-        }
-
-        // Add the host
-        $sURL .= $aURL['host'];
-
-        // Add the port if needed
-        if (!empty($aURL['port']) && (($aURL['scheme'] == 'http' && $aURL['port'] != 80) || ($aURL['scheme'] == 'https' && $aURL['port'] != 443))) {
-            $sURL .= ':' . $aURL['port'];
-        }
-
-        // Add the path and the query string
-        $sURL .= $aURL['path'] . ($aURL['query'] ?? '');
-
-        // Clean up
-        unset($aURL);
-        return $sURL;
     }
 
     /**
